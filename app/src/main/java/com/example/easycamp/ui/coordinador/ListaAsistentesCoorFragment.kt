6504a8b9bd5c9d2f8deb0623347b335ca9bf6e6f
Angela <uo284342@uniovi.es>
2024-01-12 05:19:21 +0100

    lista de asistentes coordinador no funciona

---
+++ app/src/main/java/com/example/easycamp/ui/coordinador/ListaAsistentesCoorFragment.kt
@@ -0,0 +1,70 @@
+package com.example.easycamp.ui.coordinador
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.easycamp.R
+import com.example.easycamp.domain.CampamentoDto
+import com.example.easycamp.ui.buscadorCliente.ListaHijosAdapter
+import com.example.easycamp.ui.buscadorCliente.RecyclerClienteFragment
+import com.example.easycamp.util.DBHelper
+
+private const val ARG_PARAM1 = "idCampamento"
+
+class ListaAsistentesCoorFragment : Fragment() {
+
+    private lateinit var service: DBHelper
+    private lateinit var recyclerCamp: RecyclerView
+    private var campamentoDto: Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            campamentoDto = it.getLong(ARG_PARAM1)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_lista_asistentes_coor, container, false)
+
+        service = DBHelper(context)
+        recyclerCamp = view.findViewById(R.id.recyclerViewAsistentes)
+
+        val listaDeHijos = service.obtenerInscritosDeCampamento(campamentoDto).toMutableList()
+
+        val adapterHijos = ListaHijosAdapter()
+        adapterHijos.submitList(listaDeHijos)
+
+        recyclerCamp.apply {
+            setHasFixedSize(true)
+            adapter = adapterHijos
+
+        }
+
+        recyclerCamp.layoutManager = LinearLayoutManager(activity)
+
+        Log.d("MiApp", "Se cargo la lista de asistentes en coordinador")
+
+        return view
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(idCampamento: Long) =
+                ListaAsistentesCoorFragment().apply {
+                    arguments = Bundle().apply {
+                        putLong(ARG_PARAM1, idCampamento)
+                    }
+                }
+    }
+
+
+}
